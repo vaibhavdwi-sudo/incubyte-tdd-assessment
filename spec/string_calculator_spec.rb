de1b941abddc1c112ea59a864c1b5473a451d862
vaibhav <vaibavdwi@gmail.com>
2025-02-20 15:18:06 +0530

    Test: Add test case for newline as delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -19,4 +19,7 @@ RSpec.describe StringCalculator do
     expect(StringCalculator.add("1,2,3,4")).to eq(10)
   end
 
+  it "handles newlines as delimiters" do
+    expect(StringCalculator.add("1\n2,3")).to eq(6)
+  end
 end
