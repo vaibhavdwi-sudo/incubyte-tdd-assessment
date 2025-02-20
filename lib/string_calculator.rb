9b1a2b991938ff6c4478ca4378a98722bc1fd983
vaibhav <vaibavdwi@gmail.com>
2025-02-20 15:26:17 +0530

    Feat: Throw exception for negative numbers with details

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,17 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2..].split("\n", 2)
+    else
+      delimiter = /,|\n/
+    end
+
+    num_list = numbers.split(delimiter).map(&:to_i)
+    negatives = num_list.select { |num| num < 0 }
+
+    raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+
+    num_list.sum
   end
 end
